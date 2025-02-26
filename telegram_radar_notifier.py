38bb97c2b7bc9d0b937f45ef030bf8ceda9c4d3c
Unai3105 <63521650+Unai3105@users.noreply.github.com>
2025-02-26 17:48:34 +0100

    Update telegram_radar_notifier.py

--- telegram_radar_notifier.py
+++ telegram_radar_notifier.py
@@ -192,7 +192,7 @@ def extraer_canvas(driver):
         logging.info("Canvas desplazado a la vista.")
 
         # Esperar un momento para asegurarnos de que todo se haya renderizado
-        time.sleep(2)
+        time.sleep(20)
 
         # Tomar la captura de pantalla completa como objeto binario (para no guardarla)
         screenshot = driver.get_screenshot_as_png()
